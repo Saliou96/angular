8492f76ceec6f37e7af58cd0065acaf05131d2f1
Serigne Saliou Mbacké NDIAYE <serignesalioumbackendiaye@esp.sn>
2023-04-12 16:48:33 +0000

    Aaa

--- crudStatic/src/app/components/header/header.component.ts
+++ crudStatic/src/app/components/header/header.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  cheminImage:any = "F:/work/angular/crudStatic/src/assets/im.jpeg";
 }
