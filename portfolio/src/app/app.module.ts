9b744ab1e328ebb09ebc2fa0caa897ac78722c64
Serigne Saliou Mbacké NDIAYE <68548037+Saliou96@users.noreply.github.com>
2023-05-02 13:08:15 +0000

    update

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -1,13 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MinibarComponent} from './components/minibar/minibar.component';
+import {MainComponent} from './components/main/main.component';
+import {FooterComponent} from "./sections/footer/footer.component";
+import { HomeComponent } from './sections/home/home.component';
+import { AboutComponent } from './sections/about/about.component';
+import { ServicesComponent } from './sections/services/services.component';
+import { PortfolioComponent } from './sections/portfolio/portfolio.component';
+import { ContactsComponent } from './sections/contacts/contacts.component';
+import { TestimonialsComponent } from './sections/testimonials/testimonials.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    MinibarComponent,
+    MainComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    ContactsComponent,
+    TestimonialsComponent,
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule
@@ -15,4 +33,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ portfolio/src/app/components/minibar/minibar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-minibar',
+  templateUrl: './minibar.component.html',
+  styleUrls: ['./minibar.component.css']
+})
+export class MinibarComponent {
+
+}
